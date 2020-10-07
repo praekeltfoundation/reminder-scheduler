94cb514533ae35d075d38766f226eb783edeb2dd
Kaitlyn Crawford <kaitlyn@praekelt.com>
2020-10-07 20:26:06 +0200

    Add fields to schedule list view in admin

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import ReminderSchedule, ReminderContent
 
-admin.site.register(ReminderSchedule)
+@admin.register(ReminderSchedule)
+class ContactImportAdmin(admin.ModelAdmin):
+    list_display = ("recipient_id", "schedule_time", "sent_time", "cancelled")
+
 admin.site.register(ReminderContent)
