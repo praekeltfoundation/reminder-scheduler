0330f0196d3f61e2aa2008c38a82d089dc85e8b8
Kaitlyn Crawford <kaitlyn@praekelt.com>
2020-10-07 20:27:39 +0200

    Fix mistake in admin model name

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import ReminderSchedule, ReminderContent
 
 @admin.register(ReminderSchedule)
-class ContactImportAdmin(admin.ModelAdmin):
+class ReminderScheduleAdmin(admin.ModelAdmin):
     list_display = ("recipient_id", "schedule_time", "sent_time", "cancelled")
 
 admin.site.register(ReminderContent)
